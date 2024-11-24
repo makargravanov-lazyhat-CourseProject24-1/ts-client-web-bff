54a3aed30aea2f2de04f5d72ae71d8164e7abed2
lazyhat <sevapetrov2016p@gmail.com>
2024-11-24 21:11:33 +0300

    update service 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 
-dependencies {
+plugins {
+    alias(libs.plugins.spring.kotlin)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependencyManagement)
+}
 
+dependencies {
+    implementation(rootProject.libs.spring.boot.starter)
+    implementation(rootProject.libs.spring.webMVC)
+    implementation(rootProject.libs.kotlin.reflect)
+    implementation(rootProject.projects.tsBackendCommon)
 }
