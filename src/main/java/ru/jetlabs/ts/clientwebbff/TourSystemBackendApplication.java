f097b837867acf6f92db00126ed0c5c63c3e466a
lazyhat <sevapetrov2016p@gmail.com>
2024-11-24 21:23:07 +0300

    update to java

---
+++ src/main/java/ru/jetlabs/ts/clientwebbff/TourSystemBackendApplication.java
@@ -0,0 +1,12 @@
+package ru.jetlabs.ts.clientwebbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourSystemBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TourSystemBackendApplication.class, args);
+	}
+}
+
