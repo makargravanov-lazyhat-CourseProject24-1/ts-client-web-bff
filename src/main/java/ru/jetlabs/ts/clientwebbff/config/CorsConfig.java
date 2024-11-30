b855d741bc945d599e6dc60af6fbb0cafce60411
Alex <makargravanov@gmail.com>
2024-11-30 21:24:50 +0300

    allow creds in CORS

--- src/main/java/ru/jetlabs/ts/clientwebbff/config/CorsConfig.java
+++ src/main/java/ru/jetlabs/ts/clientwebbff/config/CorsConfig.java
@@ -9,6 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://tour.lazyhat.ru")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
