268c681fda9849f7711102da6ebbed5bd73f85a4
lazyhat <sevapetrov2016p@gmail.com>
2024-12-03 19:07:07 +0300

    try to fix cors 2

--- src/main/java/ru/jetlabs/ts/clientwebbff/TourSystemBffApplication.java
+++ src/main/java/ru/jetlabs/ts/clientwebbff/TourSystemBffApplication.java
@@ -3,14 +3,29 @@ package ru.jetlabs.ts.clientwebbff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
-@ComponentScan("ru.jetlabs.ts.clientwebbff.config")
 public class TourSystemBffApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TourSystemBffApplication.class, args);
+	}
+
+	@Bean
+	public static WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings( CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173", "https://tour.lazyhat.ru")
+						.allowedMethods("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
 
