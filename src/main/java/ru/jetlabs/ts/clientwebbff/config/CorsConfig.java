a0a01ff30c84b582bfe84677b31aa40da2f2a9ee
lazyhat <sevapetrov2016p@gmail.com>
2024-12-03 20:12:32 +0300

    try to fix cors

--- src/main/java/ru/jetlabs/ts/clientwebbff/config/CorsConfig.java
+++ src/main/java/ru/jetlabs/ts/clientwebbff/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://tour.lazyhat.ru")
                 .allowedMethods("*")
+                .allowedHeaders("Cookie", "Set-Cookie")
                 .allowCredentials(true);
     }
 }
