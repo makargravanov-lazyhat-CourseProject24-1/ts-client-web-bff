56f9777809355174d93f8790dc663e5e6796bf50
lazyhat <sevapetrov2016p@gmail.com>
2024-12-03 18:49:38 +0300

    try to fix cors

--- src/main/java/ru/jetlabs/ts/clientwebbff/TourSystemBffApplication.java
+++ src/main/java/ru/jetlabs/ts/clientwebbff/TourSystemBffApplication.java
@@ -3,9 +3,11 @@ package ru.jetlabs.ts.clientwebbff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan("ru.jetlabs.ts.clientwebbff.config")
 public class TourSystemBffApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TourSystemBffApplication.class, args);
