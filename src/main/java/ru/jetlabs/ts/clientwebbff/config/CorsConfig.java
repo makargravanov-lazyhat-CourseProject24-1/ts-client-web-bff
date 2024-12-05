3e7f00fc77d7d59c970d67860727be254b1eb22f
Alex <makargravanov@gmail.com>
2024-12-05 05:37:20 +0300

    try to fix cors

--- src/main/java/ru/jetlabs/ts/clientwebbff/config/CorsConfig.java
+++ src/main/java/ru/jetlabs/ts/clientwebbff/config/CorsConfig.java
@@ -1,18 +1,27 @@
 package ru.jetlabs.ts.clientwebbff.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+import java.util.Arrays;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("Cookie", "Set-Cookie")
-                .allowCredentials(true);
+@Configuration
+public class CorsConfig extends org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.addAllowedOrigin("https://tour.lazyhat.ru");
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
     }
 }
