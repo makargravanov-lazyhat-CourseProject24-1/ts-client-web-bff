5ee3d3cbb8820023a764015f5f65684b90fedd5d
Alex <makargravanov@gmail.com>
2024-12-26 22:14:28 +0300

    fix

--- src/main/java/ru/jetlabs/ts/clientwebbff/client/tours/TourClient.java
+++ src/main/java/ru/jetlabs/ts/clientwebbff/client/tours/TourClient.java
@@ -4,9 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @FeignClient(name = "ts-tour-processing-service", url = "http://ts-tour-processing-service:8080/ts-tour-processing-service/api/v1/tour")
 public interface TourClient {
 
     @GetMapping
-    ResponseEntity<Tour> getAll();
+    ResponseEntity<List<Tour>> getAll();
 }
